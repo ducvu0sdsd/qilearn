aeb3957625aea1d900abeead485df3ac8fe36896
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-25 23:13:02 +0700

    upload

--- src/components/publicPage/ThirdSection.tsx
+++ src/components/publicPage/ThirdSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ThirdSection = () => {
     return (
-        <section className='w-full py-[2.5rem] rounded-[1rem] bg-[#4dac96] my-10 flex items-center justify-center text-white'>
+        <section className='w-full py-[2.5rem] rounded-[1rem] bg-[#4dac96] my-2 sm:my-10 flex items-center justify-center text-white'>
             <img src='/book.png' className='mx-[1rem] w-[40%]' />
             <div className='p-[10px] sm:p-[2rem] w-[50rem]'>
                 <h2 className=' text-[20px] leading-[30px] mb-3 sm:text-[2.5rem] sm:leading-[50px] sm:my-5 font-bold font-poppins'>
