7dfb3735f662c88480203048d3c6c0a4adacaf47
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2024-01-01 00:23:26 +0700

    upload

--- src/app/auth-page/sign-in/page.tsx
+++ src/app/auth-page/sign-in/page.tsx
@@ -15,6 +15,7 @@ export interface UserSignIn {
 
 const AuthPage = () => {
     const { datas, handles } = useContext(ThemeContext) || {}
+    const [loading, setLoading] = useState<{ normal: boolean, google: boolean, github: boolean }>({ normal: false, google: false, github: false })
     const [userSignIn, setUserSignIn] = useState<UserSignIn>({
         username: '',
         password: ''
@@ -23,6 +24,7 @@ const AuthPage = () => {
 
     const handleSignIn = () => {
         const { username, password } = userSignIn
+        setLoading({ ...loading, normal: true })
         api({ path: '/auth/sign-in', body: { username, password }, type: TypeHTTP.POST })
             .then(res => {
                 const result: any = res
@@ -35,11 +37,13 @@ const AuthPage = () => {
                     console.log(error)
                 }
                 setTimeout(() => {
+                    setLoading({ ...loading, normal: false })
                     router.push('/home-page')
                 }, 1500);
             })
             .catch(res => {
                 handles?.handleSetNotification({ message: `UserName Or Password don't exists in the system`, status: StatusToast.FAIL })
+                setLoading({ ...loading, normal: false })
             })
     }
 
@@ -63,6 +67,10 @@ const AuthPage = () => {
                 if (!isSignIn) {
                     const { name, email, image } = session.user
                     setIsSignIn(true)
+                    if (image?.includes('google'))
+                        setLoading({ ...loading, google: true })
+                    else if (image?.includes('github'))
+                        setLoading({ ...loading, github: true })
                     api({ path: '/auth/sign-in', body: { username: email, password: '' }, type: TypeHTTP.POST })
                         .then(res => {
                             const result: any = res
@@ -77,6 +85,10 @@ const AuthPage = () => {
                                     console.log(error)
                                 }
                                 setTimeout(() => {
+                                    if (image?.includes('google'))
+                                        setLoading({ ...loading, google: false })
+                                    else if (image?.includes('github'))
+                                        setLoading({ ...loading, github: false })
                                     router.push('/home-page')
                                 }, 1500);
                             }
@@ -84,6 +96,10 @@ const AuthPage = () => {
                         .catch(res => {
                             handles?.handleSetNotification({ message: "Google account does not exist in the system", status: StatusToast.FAIL })
                             setIsSignIn(false)
+                            if (image?.includes('google'))
+                                setLoading({ ...loading, google: false })
+                            else if (image?.includes('github'))
+                                setLoading({ ...loading, github: false })
                         })
                 }
             }
@@ -107,7 +123,18 @@ const AuthPage = () => {
                 <button
                     onClick={() => handleSignIn()}
                     className='hover:scale-[1.02] transition my-[0.5rem] font-semibold rounded-[10px] py-[12px] text-[white] bg-[#241d49]'>
-                    Sign In</button>
+                    {
+                        !loading.normal ?
+                            <>Sign In</>
+                            :
+                            <div role="status">
+                                <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                </svg>
+                            </div>
+                    }
+                </button>
                 <a className='cursor-pointer w-full text-center text-[14px]'>Forgot Password?</a>
                 <div className='my-2 flex items-center justify-center'>
                     <div className='h-[1px] w-[40%] bg-black rounded-[10px]' />
@@ -121,16 +148,42 @@ const AuthPage = () => {
                         signIn('google')
                     }}
                     className='hover:scale-[1.02] transition text-[15px] my-[5px] justify-center py-[10px] rounded-[10px] from-[#ffffffac] to-[#ffffff45] bg-gradient-to-br flex items-center text-black font-semibold'>
-                    <i className='bx bxl-google text-[2rem] mr-2'></i>
-                    Continue With Google</button>
+                    {
+                        !loading.google ?
+                            <>
+                                <i className='bx bxl-google text-[2rem] mr-2'></i>
+                                Continue With Google
+                            </>
+                            :
+                            <div role="status">
+                                <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                </svg>
+                            </div>
+                    }
+                </button>
                 <button
                     onClick={() => {
                         Cookies.set('is-first', 'vutienduc')
                         signIn('github')
                     }}
                     className='hover:scale-[1.02] transition text-[15px] my-[5px] justify-center py-[10px] rounded-[10px] from-[#ffffffac] to-[#ffffff45] bg-gradient-to-br flex items-center text-black font-semibold'>
-                    <i className='bx bxl-github text-[2rem] mr-2'></i>
-                    Continue With GitHub</button>
+                    {
+                        !loading.github ?
+                            <>
+                                <i className='bx bxl-github text-[2rem] mr-2'></i>
+                                Continue With GitHub
+                            </>
+                            :
+                            <div role="status">
+                                <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                </svg>
+                            </div>
+                    }
+                </button>
                 <p className='w-full text-center mt-[2.0rem]'>
                     Don&#39;t have an account? <Link href={'sign-up'}><span className='font-bold underline cursor-pointer'>Sign Up</span></Link></p>
             </div>
