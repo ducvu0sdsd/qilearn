404840e2107895a9c5767caaaf8405e1a79a80ce
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-31 16:54:13 +0700

    upload

--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -10,14 +10,14 @@ const HomePage = () => {
     const { datas, handles } = useContext(ThemeContext) || {}
     Cookies.remove('is-first')
     return (
-        <div className='px-[1.5rem] sm:px-[4rem] py-[60px]'>
+        <div className='px-[1rem] sm:px-[4rem] py-[60px]'>
             <PrivateNavbar />
             <section className='w-full h-auto mt-[2rem]'>
                 <motion.h2
                     initial={{ y: '-50px', opacity: 0 }}
                     animate={{ y: 0, opacity: 1, transition: { duration: 0.7 } }}
                     className='font-bold text-[27px] md:text-[1.5rem] font-poppins my-3 text-[#2c2c2c]'>Learn English</motion.h2>
-                <div className='px-[1.5rem] grid-cols-1 md:grid-cols-4 md:px-[2rem] grid gap-x-5 gap-y-3 w-full'>
+                <div className='px-[1rem] grid-cols-1 md:grid-cols-4 md:px-[2rem] grid gap-x-5 gap-y-3 w-full'>
                     <NodeList url='/practice-vocabulary.png' />
                     <NodeList url='/practice-listen.png' />
                     <NodeList url='/practice-grammar.png' />
@@ -30,7 +30,7 @@ const HomePage = () => {
                     initial={{ y: '-50px', opacity: 0 }}
                     animate={{ y: 0, opacity: 1, transition: { duration: 0.7 } }}
                     className='font-bold text-[27px] md:text-[1.5rem] font-poppins my-3 text-[#2c2c2c]'>Other</motion.h2>
-                <div className='px-[1.5rem] grid-cols-1 md:grid-cols-4 md:px-[2rem] grid gap-x-5 gap-y-3 w-full'>
+                <div className='px-[1rem] grid-cols-1 md:grid-cols-4 md:px-[2rem] grid gap-x-5 gap-y-3 w-full'>
                     <NodeList url='/note-management.png' />
                     <NodeList url='/study-time-management.png' />
                     <NodeList url='/to-do-list.png' />
