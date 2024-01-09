039f8481020dfb8e7270642a2084978c243c10bc
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2024-01-09 23:06:18 +0700

    upload

--- src/components/homePage/nodeList.tsx
+++ src/components/homePage/nodeList.tsx
@@ -2,14 +2,18 @@ import { motion } from 'framer-motion'
 import Link from 'next/link'
 import React from 'react'
 
-const NodeList = ({ urlImage, url }: { url: string, urlImage: string }) => {
+const NodeList = ({ urlImage, url, reload }: { url: string, urlImage: string, reload: boolean }) => {
     return (
         <motion.div
             initial={{ x: '50px', opacity: 0 }}
             animate={{ x: 0, opacity: 1, transition: { duration: 0.7 } }}
 
             className='bg-black rounded-xl overflow-hidden cursor-pointer'>
-            <Link href={url}><img src={urlImage} width={'100%'} /></Link>
+            {reload ?
+                <a href={url}><img src={urlImage} width={'100%'} /></a>
+                :
+                <Link href={url}><img src={urlImage} width={'100%'} /></Link>
+            }
         </motion.div>
     )
 }
